92f7967d66fcd2d490bb6523a64e7d2311ce4649
asudonp <antoniosp@iescastelar.com>
2022-10-06 10:21:35 +0200

    Modificacion PlayerViewModel.java

--- app/src/main/java/com/jparral/shortdamgames10/viewmodel/PlayerViewModel.java
+++ app/src/main/java/com/jparral/shortdamgames10/viewmodel/PlayerViewModel.java
@@ -15,9 +15,6 @@ public class PlayerViewModel extends ViewModel{
         }
         return player1;
     }
-    public void setPlayer1(Player player1){
-        this.player1 = player1;
-    }
 
     public Player getPlayer2() {
         if(player1==null){
@@ -25,8 +22,4 @@ public class PlayerViewModel extends ViewModel{
         }
         return player2;
     }
-    public void setPlayer2(Player player2){
-        this.player1 = player1;
-    }
-
 }
