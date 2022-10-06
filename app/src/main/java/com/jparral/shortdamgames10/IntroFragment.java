563cab8b0c0bbd2a8a823195b164e448b0014eea
jorgeparra <jorgeparra@iescastelar.com>
2022-10-06 10:42:24 +0200

    a

--- app/src/main/java/com/jparral/shortdamgames10/IntroFragment.java
+++ app/src/main/java/com/jparral/shortdamgames10/IntroFragment.java
@@ -7,58 +7,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link IntroFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.jparral.shortdamgames10.entities.Player;
+import com.jparral.shortdamgames10.viewmodel.GameViewModel;
+import com.jparral.shortdamgames10.viewmodel.PlayerViewModel;
+
 public class IntroFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button btn_Cambio;
+    PlayerViewModel mplayer;
+    //TextView jugador;
 
     public IntroFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IntroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static IntroFragment newInstance(String param1, String param2) {
-        IntroFragment fragment = new IntroFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_intro, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        btn_Cambio =getView().findViewById(R.id.btn_cambio);
+        Player player1= mplayer.getPlayer1();
+        btn_Cambio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               loadFragment2(new GameFragment());
+            }
+        });
+
+        }
+    private void loadFragment2(Fragment fragmento){
+        getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.f_container,fragmento)
+                .addToBackStack(null)
+                .commit();
     }
 }
